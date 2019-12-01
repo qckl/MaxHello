4af636df27455ebf3abd2772ab3b4ec8194ea206
qckl <sobakogavgav@gmail.com>
2019-12-01 22:47:28 +0300

    Поправка

---
+++ untitled2/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
+class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("Hello world!");
+    }
+}
